01a081abef286751ff6d605206e2e24ce71f5408
letocollection <leonard@letocollection.com>
2016-10-02 04:06:29 -0700

    blah

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-var express = require("express");
+var connection = require('./config/connection.js');
+var orm = require('./config/orm.js');
+var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+	res.render(console.log("how the fuck"));
+
+});
