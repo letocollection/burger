563ef2e20ed2c91fc74a336c5c6e95d3b00dec9f
letocollection <leonard@letocollection.com>
2016-10-04 02:59:35 -0700

    added app.get

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var connection = require('./config/connection.js');
 var orm = require('./config/orm.js');
 var express = require('express');
 var app = express();
+var PORT = 8080;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
@@ -9,12 +10,27 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// static directory
+app.use(express.static(process.cwd() + './public'));
+
+var path = require('path');
+
+app.listen(PORT, function(){
+	console.log('App listening on PORT ' + PORT);
+})
+
+
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res){
-	res.render(console.log("how the fuck"));
+	res.sendFile(path.join(__dirname + '/public/test.html'));
 
 });
