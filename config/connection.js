47aa23fa14c2a14c5e699350520224c0fd76d04a
letocollection <leonard@letocollection.com>
2016-10-12 19:15:08 -0700

    help

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,38 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'Superman1',
-	database: 'burgers_db'
-});
+// var connection = mysql.createConnection({
+// 	port: 3306,
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'Superman1',
+// 	database: 'burgers_db'
+// });
+
+// connection.connect(function (err) {
+// 	if (err) {
+// 		console.error('error connecting: ' + err.stack);
+// 		return;
+// 	}
+// 	console.log('connected as id ' + connection.threadId);
+// });
+
+// module.exports = connection;
+
+//this is how you get it on heroku
+
+var mysql = require('mysql');										// 	when deploying to heroku jawsdb, must use jawsdb_url codes
+var connection; 													// 	var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) { 										// 	port: 3306,
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {		
+	connection = mysql.createConnection({
+		host : 'localhost',											// 	host: 'localhost',
+		user : 'root',												// 	user: 'root',
+		password : "Superman1", 										// 	password: 'pa55w0rd',
+		database : 'burgers_db' 									// 	database: 'burgers_db'
+	});
+};																	// });
 
 connection.connect(function (err) {
 	if (err) {
--- server.js
+++ server.js
@@ -1,54 +1,29 @@
-var connection = require('./config/connection.js');
-// var orm = require('./config/orm.js');
 var express = require('express');
-var app = express();
-var PORT = 8080;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+var app = express();
+
+// static directory
+app.use(express.static(process.cwd() + './public'));
+
 
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-// static directory
-app.use(express.static(process.cwd() + './public'));
-
-var path = require('path');
-
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
-})
-
-
-
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-	connection.query('SELECT * FROM burgers;', function (err, data) {
-		if (err) throw err;
-		res.render('index', {
-			burgers: data
-			
-		});
-		console.log(data);
-	});
-});
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
-app.post('/create', function(req, res) {
+// var port = 8080;
 
-	connection.query('INSERT INTO burgers (burger_name, devoured) VALUES (?, false);', [req.body.burger_name], function (err, result){
-		if (err) throw err;
-		res.redirect('/');
-	});
-
-});
-
-
-
+var port = process.env.PORT || 8080;
+app.listen(port);
